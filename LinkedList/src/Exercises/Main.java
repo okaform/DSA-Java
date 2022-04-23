58a8c9e7bee186270f0b644bd8cc2af68128337e
okaform <Oka17002@byui.edu>
2022-04-22 22:43:12 -0600

    Finished Linked List. Moving on to Queue!

--- LinkedList/src/Exercises/Main.java
+++ LinkedList/src/Exercises/Main.java
@@ -4,20 +4,34 @@ public class Main {
 
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
-        ll.createLL(1);
-        ll.insertNode(2);
-        ll.insertNode(56);
-        ll.insertNode(4);
+        ll.createLL(3);
+        ll.insertNode(1);
         ll.insertNode(5);
+        ll.insertNode(9);
 
+        LinkedList ll2 = new LinkedList();
+        ll2.createLL(2);
+        ll2.insertNode(4);
+        ll2.insertNode(6);
+
+        removeDups q = new removeDups();
+        q.addSameNode(ll, ll2, 7);
+        q.addSameNode(ll, ll2, 2);
+        q.addSameNode(ll, ll2, 1);
+
+        Node intersection = q.findIntersection(ll, ll2);
+        System.out.println(intersection.value);
+
+
+    /*    ll2.traverseLL();
         ll.traverseLL();
 
 
-        //removeDups.dups(ll);
+        removeDups.sumList(ll, ll2);
+        LinkedList q = new LinkedList();
+        q = removeDups.sumListProf(ll, ll2);
 
-        ll.traverseLL();
-
-        System.out.println(removeDups.nthToLast(ll, 30));
+        q.traverseLL();*/
 
 
     }
--- LinkedList/src/Exercises/removeDups.java
+++ LinkedList/src/Exercises/removeDups.java
@@ -30,7 +30,6 @@ public class removeDups {
     public static int nthToLast(LinkedList list, int number) {
         Node currNode = list.head;
         Node prev = null;
-        System.out.println("");
 
         if (number >= list.size) {
             return currNode.value;
@@ -48,4 +47,121 @@ public class removeDups {
         }
 
     }
+
+    public static LinkedList partition(LinkedList list, int value) {
+        Node currNode = list.head;
+        list.tail = list.head; //point the head to the tail
+
+        while (currNode != null) {
+            Node next = currNode.next;
+            if(currNode.value < value) {
+                currNode.next = list.head; //insert at the beginning of list
+                list.head = currNode;
+            }
+            else {
+               list.tail.next = currNode;
+               list.tail = currNode;
+            }
+            currNode = next;
+        }
+       // list.tail.next = null;
+         return  list;
+    }
+
+/*This function takes two lists and returns the addition of the reversed of both lists*/
+    public static LinkedList sumList(LinkedList ll, LinkedList ll2) {
+        String string1 = "";
+        String string2 = "";
+        Node tempNode = ll.head;
+        Node tempNode2 = ll2.head;
+
+        for (int i = 0; i < ll.size; i++) {
+            string1 += tempNode.value;
+            string2 += tempNode2.value;
+
+            tempNode = tempNode.next;
+            tempNode2 = tempNode2.next;
+        }
+        StringBuilder reversed1 = new StringBuilder(string1);
+        StringBuilder reversed2 = new StringBuilder(string2);
+        reversed1.reverse();
+        reversed2.reverse();
+
+        int added = Integer.parseInt(reversed1.toString()) + Integer.parseInt(reversed2.toString());
+
+        String flippedString = String.valueOf(added);
+
+        LinkedList flipped = new LinkedList();
+        for (int i = 0; i < flippedString.length(); i++) {
+            System.out.println("Flipped ds " +flippedString.charAt(i));
+            flipped.insertNode(Integer.parseInt(String.valueOf(flippedString.charAt(i))));
+        }
+        flipped.traverseLL();
+        return flipped;
+    }
+
+    public static LinkedList sumListProf(LinkedList ll, LinkedList ll2) {
+        Node n1 = ll.head;
+        Node n2 = ll2.head;
+        int carry = 0;
+        LinkedList resultLL = new LinkedList();
+        while(n1 != null || n2 != null) {
+            int result = carry;
+            if (n1 != null) {
+                result += n1.value;
+                n1 = n1.next;
+            }
+            if (n2 != null) {
+                result += n2.value;
+                n2 = n2.next;
+            }
+            resultLL.insertNode(result%10); //to get the last digit
+            carry = result/10; //this carries the result over to the next function
+        }
+        return resultLL;
+    }
+
+    //Intersection method
+    //get the kthNode
+    public static Node getKthNode(Node head, int k) {
+        Node current = head;
+        while(k > 0 && current != null) {
+            current = current.next;
+            k--;
+        }
+        return current;
+    }
+    public static Node findIntersection(LinkedList ll, LinkedList ll2) {
+        if (ll.head == null || ll2.head == null) return null;
+        if (ll.tail != ll2.tail) return null;// their tails must be the same if they are intersecting
+        Node shorter = new Node();
+        Node longer = new Node();
+
+        if (ll.size > ll2.size) {//find the longer size of the two
+            longer = ll.head;
+            shorter = ll2.head;
+        }
+        else {
+            longer = ll2.head;
+            shorter = ll.head;
+        }
+        //ignore some elements of the longer lists
+        longer = getKthNode(longer, Math.abs(ll.size - ll2.size));
+        while (shorter != longer) {
+            shorter = shorter.next;
+            longer = longer.next;
+        }
+        return longer;
+    }
+    //add same node
+    public static void addSameNode(LinkedList ll, LinkedList ll2, int nodevalue) {
+        Node newNode = new Node();
+        newNode.value = nodevalue;
+        ll.tail.next = newNode;
+        ll.tail = newNode;
+        ll2.tail.next = newNode;
+        ll2.tail = newNode;
+
+    }
+
 }
